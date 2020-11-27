637db226ca757e96fb2fd9b6ced50995af90b377
Yongwoo, Lee <leeyw910205@gmail.com>
2020-11-27 18:59:23 +0900

    feat: 관찰자 패턴 사용, eventListener를 이용한 pub sub 패턴

---
+++ toby01/src/test/java/me/joeylee/study/reactive/Toby01ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joeylee.study.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toby01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
